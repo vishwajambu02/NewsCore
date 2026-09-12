10c32ba103586c56c4c185651061089ec6270432
Vishwa Jambu <vishwaj@myyahoo.com>
2026-09-12 20:49:22 +0530

    Update decorators.py

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,4 +1,4 @@
-﻿from functools import wraps
+from functools import wraps
 from flask import session, redirect, url_for, request
 
 
@@ -17,3 +17,23 @@ def current_user():
     if not uid:
         return None
     return User.query.get(uid)
+
+
+def retry_on_db_error(f):
+    """
+    Neon's free-tier Postgres can drop an idle/serverless connection
+    mid-request (shows up as 'SSL error: bad record type' or similar).
+    pool_pre_ping catches most stale connections at checkout, but not
+    ones that die mid-query. Retry the whole view once after rolling
+    back the broken session, instead of showing the user a 500.
+    """
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        from sqlalchemy.exc import OperationalError
+        from extensions import db
+        try:
+            return f(*args, **kwargs)
+        except OperationalError:
+            db.session.rollback()
+            return f(*args, **kwargs)
+    return wrapper
